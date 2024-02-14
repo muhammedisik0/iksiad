cbae2b2985081a26faef028fbce45c30667f73ef
muhammedisik0 <muhammedisik610@gmail.com>
2024-02-14 18:25:08 +0300

    Make navbar responsive

--- includes/header/components/navbar.php
+++ includes/header/components/navbar.php
@@ -1,48 +1,60 @@
 <nav class="navbar navbar-expand-lg navbar-dark">
-  <a class="navbar-brand text-white d-flex align-items-center p-0 me-2" id="logo" href="?page=home-page">
-    <img class="me-2" src="img/logo-light.png" alt="Logo">
-    <span>İKSİAD</span>
-  </a>
-  <ul class="navbar-nav">
-    <li class="nav-item">
-      <a class="nav-link text-white" id="home-page" aria-current="page" href="?page=home-page">Anasayfa</a>
-    </li>
-    <li class="nav-item dropdown">
-      <a class="nav-link text-white" href="#" id="corporate">
-        Kurumsal
+  <div class="container-fluid">
+
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+      <a class="navbar-brand text-white d-flex align-items-center p-0 me-2" id="logo" href="?page=home-page">
+        <img class="me-2" src="img/logo-light.png" alt="Logo">
+        <span>İKSİAD</span>
       </a>
-      <ul class="dropdown-menu">
-        <li><a class="dropdown-item corporate" href="?page=corporate/about">Hakkımızda</a></li>
-        <li><a class="dropdown-item corporate" href="?page=corporate/chairman">Başkan</a></li>
-        <li><a class="dropdown-item corporate" href="?page=corporate/council">Yönetim Kurulu</a></li>
-        <li><a class="dropdown-item corporate" href="?page=corporate/members">Üyelerimiz</a></li>
-        <li><a class="dropdown-item corporate" href="?page=corporate/member-feedback">Üye Görüşleri</a></li>
-      </ul>
-    </li>
-    <li class="nav-item dropdown">
-      <a class="nav-link text-white" href="#" id="industries">
-        Sektörler
-      </a>
-      <ul class="dropdown-menu">
-        <li><a class="dropdown-item industries" href="?page=industries/electrical-electronic">Elektrik -
-            Elektronik</a>
+
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link text-white" id="home-page" aria-current="page" href="?page=home-page">Anasayfa</a>
         </li>
-        <li><a class="dropdown-item industries" href="?page=industries/construction-machinery">İnşaat - Makine</a>
+        <li class="nav-item dropdown">
+          <a class="nav-link text-white" href="#" id="corporate">
+            Kurumsal
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item corporate" href="?page=corporate/about">Hakkımızda</a></li>
+            <li><a class="dropdown-item corporate" href="?page=corporate/chairman">Başkan</a></li>
+            <li><a class="dropdown-item corporate" href="?page=corporate/council">Yönetim Kurulu</a></li>
+            <li><a class="dropdown-item corporate" href="?page=corporate/members">Üyelerimiz</a></li>
+            <li><a class="dropdown-item corporate" href="?page=corporate/member-feedback">Üye Görüşleri</a></li>
+          </ul>
         </li>
-        <li><a class="dropdown-item industries" href="?page=industries/textile-leather">Tekstil - Deri</a></li>
+        <li class="nav-item dropdown">
+          <a class="nav-link text-white" href="#" id="industries">
+            Sektörler
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item industries" href="?page=industries/electrical-electronic">Elektrik -
+                Elektronik</a>
+            </li>
+            <li><a class="dropdown-item industries" href="?page=industries/construction-machinery">İnşaat - Makine</a>
+            </li>
+            <li><a class="dropdown-item industries" href="?page=industries/textile-leather">Tekstil - Deri</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white" href="?page=activities">Faaliyetler</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white" href="?page=events">Etkinlikler</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white" href="?page=gallery">Galeri</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white" href="?page=contact">İletişim</a>
+        </li>
       </ul>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link text-white" href="?page=activities">Faaliyetler</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link text-white" href="?page=events">Etkinlikler</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link text-white" href="?page=gallery">Galeri</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link text-white" href="?page=contact">İletişim</a>
-    </li>
-  </ul>
+    </div>
+  </div>
 </nav>
