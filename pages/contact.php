1776d61975beccbfd81524c95dfeb06240c3b2b3
Muhammed IŞIK <muhammedisik909@gmail.com>
2024-02-15 21:00:52 +0300

    Make contact page responsive

--- pages/contact.php
+++ pages/contact.php
@@ -1,12 +1,13 @@
 <div class="row" id="contact">
-    <div class="col">
-        <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3008.138905897801!2d28.803783784848136!3d41.06595624199434!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14caa59be46494b7%3A0xfd45b12ab0202223!2s%C3%87izgi%20Etiket!5e0!3m2!1str!2str!4v1707907969739!5m2!1str!2str"
-            width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"
-            referrerpolicy="no-referrer-when-downgrade">
-        </iframe>
+    <div class="col-12 col-sm-6">
+        <div class="iframe-container">
+            <iframe class="responsive-iframe"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3008.138905897801!2d28.803783784848136!3d41.06595624199434!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14caa59be46494b7%3A0xfd45b12ab0202223!2s%C3%87izgi%20Etiket!5e0!3m2!1str!2str!4v1707907969739!5m2!1str!2str"
+                allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade">
+            </iframe>
+        </div>
     </div>
-    <div class="col d-flex justify-content-center align-items-center">
+    <div class="col-12 col-sm-6 d-flex align-items-center">
         <div class="row">
             <h3 class="mb-3">İletişim Bilgileri</h3>
             <span><i class="fas fa-phone-alt me-2"></i>+90 123 456 7890</span>
